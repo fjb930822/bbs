30e339e5bb736a5b4d2738c02be5ea09d1f79691
fengjianbo <904982022@qq.com>
2018-07-09 19:49:41 +0800

    完成登录、退出、个人主页功能

---
+++ user/helper.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+from django.shortcuts import redirect
+
+def login_required(view_func):
+    def check(request):
+        uid = request.session.get('uid')
+        if uid is None:
+            return redirect('/user/login')
+        else:
+            return view_func(request)
+    return check
